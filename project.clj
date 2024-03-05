51c83a2267d8e1ba16bc68f06619c3e11525067f
GenerateMe <generateme.blog@gmail.com>
2024-03-05 16:43:59 +0100

    project descriptions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject fastmath-clustering "1.0.0"
+(defproject generateme/fastmath-clustering "1.0.0"
   :description "SMILE Clustering wrapper"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/generateme/fastmath-clustering"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
@@ -12,5 +12,7 @@
                  [org.bytedeco/openblas "0.3.10-1.5.4"]
                  [org.bytedeco/openblas-platform "0.3.10-1.5.4"]
                  [org.bytedeco/javacpp "1.5.4"]]
+  :scm {:name "git"
+        :url "https://github.com/generateme/fastmath-clustering"}
   :pedantic? false
   :profiles {:dev-codox {:codox {:source-uri "https://github.com/generateme/fastmath-clustering/blob/master/{filepath}#L{line}"}}})
